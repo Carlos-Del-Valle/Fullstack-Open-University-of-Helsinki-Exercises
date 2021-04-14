b3a76eeda330be739d4f91e7bfb1a7bde7719c7f
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-04-14 16:03:31 +1000

    Add 1.12*: anecdotes step1

--- part-1/Exercises 1.6.-1.14./my-app/src/App.js
+++ part-1/Exercises 1.6.-1.14./my-app/src/App.js
@@ -50,6 +50,7 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
     const [allClicks, setAll] = useState(0)
+    const [selected, setSelected] = useState(0)
 
     const handleGoodClick = () => {
         console.log('clicked the Good click button')
@@ -69,6 +70,20 @@ const App = () => {
         setAll(allClicks +1)
     }
 
+    const handleAnecdoteClick = () => {
+        console.log('clicked the Next anecdote button')
+        setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    ]
+
     return (
         <div>
             <h1>give feedback</h1>
@@ -79,6 +94,11 @@ const App = () => {
 
             <h1>statistics</h1>
             <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks}/>
+
+            <h1>Anecdotes</h1>
+            <p>{anecdotes[selected]}</p>
+            <Button handleClick={handleAnecdoteClick} text='Next anecdote' />
+
 
         </div>
     )
