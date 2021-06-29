2e6c2788dee60347a974696162a538dadcf0128c
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-06-29 12:49:29 +1000

    Add Exercise 2.9

--- part-2/Exercises 2.6.-2.10./my-app/src/App.js
+++ part-2/Exercises 2.6.-2.10./my-app/src/App.js
@@ -3,24 +3,30 @@ import React, { useState } from "react";
 const Person = props => {
     return (
         <>
-            {props.name}
-            {props.number}
+            {props.name} {props.number}
             <br />
         </>
     );
 };
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "-0426877076" }]);
+    const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "0426877076" }]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
+    const [nameFilter, setNameFilter] = useState("");
 
-    const names = persons.map(person => {
+    const lowerCasedNames = persons.map(person => {
         return person.name.toLocaleLowerCase();
     });
 
-    const personsList = persons.map(person => {
-        return <Person key={person.name} name={person.name} number={person.number}/>;
+    const filteredPersons = persons.filter(person => {
+        return person.name.toLocaleLowerCase().includes(nameFilter.toLowerCase());
+    });
+
+    const personsList = filteredPersons.map(person => {
+        return (
+            <Person key={person.name} name={person.name} number={person.number} />
+        );
     });
 
     const handleChangeName = event => {
@@ -31,10 +37,14 @@ const App = () => {
         setNewNumber(event.target.value);
     };
 
+    const handleChangeFilter = event => {
+        setNameFilter(event.target.value);
+    };
+
     const handleSubmit = event => {
         event.preventDefault();
 
-        if (names.includes(newName.toLocaleLowerCase())) {
+        if (lowerCasedNames.includes(newName.toLocaleLowerCase())) {
             alert(`${newName} is already added to the phonebook.`);
         } else {
             const replacer = (match, p1, p2) => {
@@ -45,7 +55,7 @@ const App = () => {
 
             const person = {
                 name: titleCasedName,
-                number: `-${newNumber}`
+                number: newNumber
             };
 
             setPersons([...persons].concat(person));
@@ -58,7 +68,11 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            Filter by Name:{" "}
+            <input
+                onChange={event => handleChangeFilter(event)} value={nameFilter}/>
             <form onSubmit={event => handleSubmit(event)}>
+                <h2>Add a new</h2>
                 <div>
                     name:{" "}
                     <input onChange={event => handleChangeName(event)} value={newName} />
@@ -71,7 +85,7 @@ const App = () => {
                     <button type="submit">add</button>
                 </div>
             </form>
-            <h2>Numbers</h2>
+            <h2>Names & Numbers</h2>
             {personsList}
         </div>
     );
