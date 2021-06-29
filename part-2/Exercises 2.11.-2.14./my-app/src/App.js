3a88da3b6bbdc3cbd769fbc159c30034dc3d9a4e
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-06-29 20:59:00 +1000

    Add boilerplate

---
+++ part-2/Exercises 2.11.-2.14./my-app/src/App.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const initialPersons = [
+  { name: "Arto Hellas", number: "040-123456" },
+  { name: "Ada Lovelace", number: "39-44-5323523" },
+  { name: "Dan Abramov", number: "12-43-234345" },
+  { name: "Mary Poppendieck", number: "39-23-6423122" }
+];
+
+const App = () => {
+  const [persons, setPersons] = useState(initialPersons);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
+
+  const lowerCasedNames = persons.map(person => {
+    return person.name.toLocaleLowerCase();
+  });
+
+  const filteredPersons = persons.filter(person => {
+    return person.name.toLocaleLowerCase().includes(nameFilter.toLowerCase());
+  });
+
+  const handleChange = (event, type) => {
+    switch (type) {
+      case "nameFilter":
+        setNameFilter(event.target.value);
+        break;
+      case "name":
+        setNewName(event.target.value);
+        break;
+      case "number":
+        setNewNumber(event.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const createNewPerson = () => {
+    const replacer = (match, p1, p2) => {
+      return p1.toUpperCase() + p2.toLocaleLowerCase();
+    };
+
+    const titleCasedName = newName.replace(/\b([a-zA-Z])(\w+)/g, replacer);
+
+    const person = {
+      name: titleCasedName,
+      number: newNumber
+    };
+
+    return person;
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (lowerCasedNames.includes(newName.toLocaleLowerCase())) {
+      alert(`${newName} is already added to the phonebook.`);
+    } else {
+      const person = createNewPerson();
+      setPersons([...persons].concat(person));
+    }
+
+    setNewName("");
+    setNewNumber("");
+  };
+
+  return (
+      <div>
+        <h2>Phonebook</h2>
+
+        <Filter
+            handleFilterChange={event => handleChange(event, "nameFilter")}
+            value={nameFilter}
+        />
+
+        <h3>Add a New:</h3>
+
+        <PersonForm
+            nameValue={newName}
+            numberValue={newNumber}
+            handleSubmit={event => handleSubmit(event)}
+            handleNameChange={event => handleChange(event, "name")}
+            handleNumberChange={event => handleChange(event, "number")}
+        />
+
+        <h3>Numbers</h3>
+
+        <Persons persons={filteredPersons} />
+      </div>
+  );
+};
+
+export default App;
