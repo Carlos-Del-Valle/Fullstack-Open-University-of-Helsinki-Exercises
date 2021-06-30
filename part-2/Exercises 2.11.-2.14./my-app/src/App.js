cd879f0b64430c45210eeed2d44b326913b3fba5
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-06-30 11:26:12 +1000

    Add Exercise 2.11

--- part-2/Exercises 2.11.-2.14./my-app/src/App.js
+++ part-2/Exercises 2.11.-2.14./my-app/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from 'axios';
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -11,10 +12,20 @@ const initialPersons = [
 ];
 
 const App = () => {
-  const [persons, setPersons] = useState(initialPersons);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [nameFilter, setNameFilter] = useState("");
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log('promise fulfilled')
+          setPersons(response.data)
+        })
+  }, [])
 
   const lowerCasedNames = persons.map(person => {
     return person.name.toLocaleLowerCase();
