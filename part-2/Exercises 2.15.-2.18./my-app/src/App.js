17dfc1c6c1091f4005b1d3adaf59b408b9504e4b
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-07-07 12:05:26 +1000

    Add Exercise: 2.18: Phonebook step10

--- part-2/Exercises 2.15.-2.18./my-app/src/App.js
+++ part-2/Exercises 2.15.-2.18./my-app/src/App.js
@@ -1,15 +1,8 @@
-import React, { useState,useEffect } from "react";
-import personService from './services/persons';
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-
-const initialPersons = [
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-];
+import personsService from "./services/persons";
 
 const App = () => {
     const [persons, setPersons] = useState([]);
@@ -18,19 +11,14 @@ const App = () => {
     const [nameFilter, setNameFilter] = useState("");
 
     useEffect(() => {
-        console.log('effect')
-        personService
-            .getAll()
-            .then(initialPersons => {
-                console.log('promise fulfilled')
-                setPersons(initialPersons)
-            })
-    }, [])
+        personsService.getAll().then(initialPersons => {
+            setPersons(initialPersons);
+        });
+    }, []);
 
-
-    const lowerCasedNames = persons.map(person => {
-        return person.name.toLocaleLowerCase();
-    });
+    // const lowerCasedNames = persons.map(person => {
+    //   return person.name.toLocaleLowerCase();
+    // });
 
     const filteredPersons = persons.filter(person => {
         return person.name.toLocaleLowerCase().includes(nameFilter.toLowerCase());
@@ -51,102 +39,117 @@ const App = () => {
                 break;
         }
     };
-
     const createNewPerson = () => {
         const replacer = (match, p1, p2) => {
             return p1.toUpperCase() + p2.toLocaleLowerCase();
         };
-
         const titleCasedName = newName.replace(/\b([a-zA-Z])(\w+)/g, replacer);
-
         const person = {
             name: titleCasedName,
             number: newNumber
         };
-
         return person;
+    };
+
+    const confirmAndUpdatePerson = () => {
+        const person = persons.find(
+            p => p.name.toLowerCase() === newName.toLowerCase()
+        );
+        const wantsToUpdate = window.confirm(
+            `${person.name} is alrady added to the phonebook, ` +
+            "replace the old number with a new one?"
+        );
+
+        if (wantsToUpdate) {
+            const updatedPerson = { ...person, number: newNumber };
+            const id = person.id;
+            return personsService
+                .update(id, updatedPerson)
+                .then(returnedPerson => {
+                    setPersons(persons.map(p => (p.id !== id ? p : returnedPerson)));
+                    return true;
+                })
+                .catch(error => {
+                    console.error(error);
+                    alert(`The person ${person.name} does not exist on the server`);
+                    setPersons(persons.filter(p => p.id !== id));
+                });
+        } else {
+            return Promise.resolve(false);
+        }
     };
 
     const handleSubmit = event => {
         event.preventDefault();
 
-        if (lowerCasedNames.includes(newName.toLocaleLowerCase())) {
-            alert(`${newName} is already added to the phonebook.`);
-        } else {
-            const person = createNewPerson();
-            personService
-                .create("http://localhost:3001/persons", person)
-                .then(response => {
-                setPersons(persons.concat(response.data));
-                setNewName("");
-                setNewNumber("");
-            });
-        }
+            const nameExists = persons.some(
+                p => p.name.toLowerCase() === newName.toLowerCase()
+            );
 
-        setNewName("");
-        setNewNumber("");
+            if (nameExists) {
+                confirmAndUpdatePerson().then(wasUpdated => {
+                    if (wasUpdated) console.log("Person Updated Successfully");
+                    else console.log("User Opted Not To Update Person");
+                });
+            } else {
+                const person = createNewPerson();
+                personsService.create(person).then(returnedPerson => {
+                    setPersons(persons.concat(returnedPerson));
+                });
+            }
+
+            setNewName("");
+            setNewNumber("");
+        };
+
+        const removePersonWithId = id => {
+            let deleted = true;
+            personsService
+                .remove(id)
+                .catch(err => {
+                    console.log(err);
+                    deleted = false;
+                })
+                .finally(() => {
+                    if (deleted) {
+                        setPersons(persons.filter(p => p.id !== id));
+                    }
+                });
+        };
+        const handleClick = (event, type) => {
+            switch (type) {
+                case "deletePerson":
+                    const id = parseInt(event.target.dataset.id);
+                    if (Number.isNaN(id) || !id) break;
+                    const person = persons.find(p => p.id === id);
+                    const wantsToDelete = window.confirm(`Delete ${person.name}?`);
+                    if (wantsToDelete) removePersonWithId(id);
+                    break;
+                default:
+                    break;
+            }
+        };
+        return (
+            <div>
+                <h2>Phonebook</h2>
+                <Filter
+                    handleFilterChange={event => handleChange(event, "nameFilter")}
+                    value={nameFilter}
+                />
+                <h3>Add a New:</h3>
+                <PersonForm
+                    nameValue={newName}
+                    numberValue={newNumber}
+                    handleSubmit={event => handleSubmit(event)}
+                    handleNameChange={event => handleChange(event, "name")}
+                    handleNumberChange={event => handleChange(event, "number")}
+                />
+                <h3>Numbers</h3>
+                <Persons
+                    persons={filteredPersons}
+                    handleClick={event => handleClick(event, "deletePerson")}
+                />
+            </div>
+        );
     };
-
-    const removePersonWithId = id => {
-        let deleted = true;
-
-        personService
-            .remove(id)
-            .catch(err => {
-                console.log(err);
-                deleted = false;
-            })
-            .finally(() => {
-                if (deleted) {
-                    setPersons(persons.filter(p => p.id !== id));
-                }
-            });
-    };
-
-    const handleClick = (event, type) => {
-        switch (type) {
-            case "deletePerson":
-                const id = parseInt(event.target.dataset.id);
-                if (Number.isNaN(id) || !id) break;
-                const person = persons.find(p => p.id === id);
-                const wantsToDelete = window.confirm(`Delete ${person.name}?`);
-                if (wantsToDelete) removePersonWithId(id);
-                break;
-            default:
-                break;
-        }
-    };
-
-
-    return (
-        <div>
-            <h2>Phonebook</h2>
-
-            <Filter
-                handleFilterChange={event => handleChange(event, "nameFilter")}
-                value={nameFilter}
-            />
-
-            <h3>Add a New:</h3>
-
-            <PersonForm
-                nameValue={newName}
-                numberValue={newNumber}
-                handleSubmit={event => handleSubmit(event)}
-                handleNameChange={event => handleChange(event, "name")}
-                handleNumberChange={event => handleChange(event, "number")}
-            />
-
-            <h3>Numbers</h3>
-
-            <Persons
-                persons={filteredPersons}
-                handleClick={event => handleClick(event, "deletePerson")}
-            />
-
-        </div>
-    );
-};
-
-export default App;
-
+    export default App;
--- part-2/Exercises 2.15.-2.18./my-app/src/services/persons.js
+++ part-2/Exercises 2.15.-2.18./my-app/src/services/persons.js
@@ -1,18 +1,20 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
+
+const create = newPerson => {
+    const request = axios.post(baseUrl, newPerson);
+
+    return request.then(response => response.data);
+};
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
-const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
-}
-
-const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
+const update = (id, newPerson) => {
+    const request = axios.put(`${baseUrl}/${id}`, newPerson);
     return request.then(response => response.data)
 }
 
@@ -26,4 +28,5 @@ export default {
     create: create,
     update: update,
     remove: remove,
+
 }
